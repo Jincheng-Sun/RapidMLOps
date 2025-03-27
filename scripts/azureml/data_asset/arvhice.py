16911819603891eca360d5a012e44e91337dfc65
Jincheng-Sun <jincheng.sun@outlook.com>
2025-03-26 23:27:21 -0400

    refactor data asset operations

--- scripts/azureml/data_asset/arvhice.py
+++ scripts/azureml/data_asset/arvhice.py
@@ -1,14 +1,18 @@
 import yaml
-import argparse
 
 from azure.ai.ml.entities import Data
 
-from utils import get_logger, get_ml_client
+from scripts.azureml.utils import get_logger, get_ml_client
 
 logger = get_logger(__name__)
 
 
-def main(data_config_path: str, subscription_id: str = None, resource_group: str = None, workspace_name: str = None):
+def main(name: str,
+         version: str,
+         subscription_id: str = None,
+         resource_group: str = None,
+         workspace_name: str = None
+         ):
     if subscription_id and resource_group and workspace_name:
         workspace_info = {"subscription_id": subscription_id,
                           "resource_group": resource_group,
@@ -17,24 +21,24 @@ def main(data_config_path: str, subscription_id: str = None, resource_group: str
     else:
         workspace_info = None
     ml_client = get_ml_client(logger_name=__name__, workspace_info=workspace_info)
-    with open(data_config_path, "r") as f:
-        data_config = yaml.safe_load(f)
-    logger.info(f"[INFO] Load Data Asset configuration: {data_config}")
-    data_asset = Data(type=data_config["type"],
-                      name=data_config["name"],
-                      version=data_config["version"],
-                      description=data_config["description"],
-                      path=data_config["path"]
-                      )
-    ml_client.data.create_or_update(data_asset)
-    logger.info(f"[INFO] Data Asset {data_asset.name}:{data_asset.version} created successfully "
+    ml_client.data.archive(name=name, version=version)
+    logger.info(f"[INFO] Data Asset {name}:{version} archived successfully "
                 f"in {ml_client.workspace_name}"
                 )
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Create a data asset in Azure Machine Learning workspace")
-    parser.add_argument("--data_config_path", type=str, required=True, help="The path to the data asset configuration.")
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Archive a data asset in Azure Machine Learning workspace")
+    parser.add_argument("--name",
+                        type=str,
+                        help="The Data Asset name."
+                        )
+    parser.add_argument("--version",
+                        type=str,
+                        help="The Data Asset version."
+                        )
     parser.add_argument("--subscription_id",
                         type=str,
                         default=None,
@@ -51,7 +55,8 @@ if __name__ == "__main__":
                         help="The name of the Azure Machine Learning workspace."
                         )
     args = parser.parse_args()
-    main(data_config_path=args.data_config_path,
+    main(name=args.name,
+         version=args.version,
          subscription_id=args.subscription_id,
          resource_group=args.resource_group,
          workspace_name=args.workspace_name
